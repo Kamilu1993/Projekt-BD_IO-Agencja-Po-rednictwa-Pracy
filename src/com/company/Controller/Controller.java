18d5ee3ea90b4b7735a66fcb1e2d199bfa7f14cc
Bajtas <bajtas@gmail.com>
2015-04-22 11:34:44 +0200

    Stworzenie klasy kontrolera aplikacji oraz implementacja wymiany informacji pomiędzy oknem logowania oraz informacji o stanie połączenia a klasą modelu.

--- src/com/company/Controller/Controller.java
+++ src/com/company/Controller/Controller.java
@@ -1,32 +1,99 @@
 package com.company.Controller;
 
 import com.company.View.Gui;
+import com.company.View.Login;
+import com.company.Model.Model;
+import com.company.View.ShowConnectionInfo;
 
-import java.awt.*;
-
+import javax.swing.*;
+import java.awt.event.ActionListener;
 
 /**
  *
  */
-public class Controller {
-    private enum Actions {CLOSE_APP};
-    private Gui MainWindow; // klasa glownego okna
-    /* Wymiary minimalne okna */
-    private int MinWidth = 600;
-    private int MinHeight = 600;
-    //------------------------//
-    public Controller() // Konstruktor kontrolera tworzacy glowne okno aplikacji
-    {
-        Dimension MinSize = new Dimension(MinWidth, MinHeight);
-        MainWindow = new Gui();
-        MainWindow.pack();
-        MainWindow.setResizable(false);
-        MainWindow.setMinimumSize(MinSize);
-        MainWindow.setVisible(true);
-        System.exit(0);
+public class Controller extends SwingWorker implements ActionListener{
+    private Login theLogin;
+    private Gui theGui;
+    private Model theModel;
+    ShowConnectionInfo info;
+
+    protected Integer doInBackground() throws Exception {
+        // Start
+        publish("Start");
+        setProgress(1);
+
+        // More work was done
+        publish("More work was done");
+        setProgress(10);
+
+        // Complete
+        publish("Complete");
+        setProgress(100);
+        return 1;
     }
-    public int getAction()
+
+    public void actionPerformed(java.awt.event.ActionEvent e){
+        JButton b = (JButton)e.getSource();
+       if(b.getText()=="Zaloguj")
+       {
+           info = new ShowConnectionInfo();
+
+           int isInputGood;
+           isInputGood = theModel.CheckInput(theLogin.GetUsername(),theLogin.GetPassword());
+
+           if(isInputGood==0){
+               final Runnable doHelloWorld = new Runnable() {
+                   public void run() {
+                       info.run();
+                       info.ShowDialog();
+                   }
+               };
+               Thread appThread = new Thread() {
+                   public void run() {
+                       try {
+                           SwingUtilities.invokeAndWait(doHelloWorld);
+                       }
+                       catch (Exception e) {
+                           e.printStackTrace();
+                       }
+                       theModel.TryToConnect();
+
+
+
+
+                       info.HideDialog();
+                   }
+               };
+               appThread.start();
+           }
+           else
+               theLogin.setErrorCode(isInputGood);
+
+       }
+
+        //JOptionPane.showMessageDialog(null, b.getText(), "InfoBox: " + "Tytul", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    //------------------------//
+    public Controller(Gui g, Login l, Model m) {
+        this.theLogin = l;
+        this.theGui = g;
+        this.theModel = m;
+
+        this.theLogin.addController(this);
+       // System.exit(0);
+    }// Konstruktor tworzący główne okno aplikacji
+    public void getAction()
     {
-        return 0;
+
+        //return 0;
+    }
+    public void ShowConnectionInfo()
+    {
+
+    }
+    public void StartApp()
+    {
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,20 @@
 package com.company;
 
 import com.company.Controller.Controller;
+import com.company.View.Login;
+import com.company.View.Gui;
+import com.company.Model.Model;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Controller MainController = new Controller();
+        Login LoginFrame = new Login();
+        Gui GuiFrame = new Gui();
+        Model MainModel = new Model();
+
+        Controller MainController = new Controller(GuiFrame, LoginFrame, MainModel);
+
+        MainController.StartApp();
     }
 }
