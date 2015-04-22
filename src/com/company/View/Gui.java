d86f7153521c0ae531ebddf4d02815581b092ac6
Bajtas <bajtas@gmail.com>
2015-04-22 11:49:35 +0200

    Implementacja połączenia z bazą oraz stworzenie klasy zarządzającej wyglądem aplikacji po pomyślnym przejściu procesu autoryzacji.

--- src/com/company/View/Gui.java
+++ src/com/company/View/Gui.java
@@ -1,60 +1,86 @@
 package com.company.View;
 
+import com.company.Main;
+
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Gui extends JDialog {
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
+/**
+ * GLOWNA KLASA TWORZACA OKNO
+ */
+public class Gui extends JFrame
+{
+    private JFrame MainFrame;
+    private String AppName;
+    private JButton BtConnectToDB;
+    private int ButtonClicked=0;
 
-    public Gui() {
-        setContentPane(contentPane);
-        setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
 
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
+    public Gui()
+    {
+        AppName = "System obslugi agencji posrednictwa pracy";
+    } // Konstruktor domyślny
+    public Gui(String name)
+    {
+        AppName = name;
+    } // Konstruktor inicjalizujacy
+    public void CreateWin() {
+        MainFrame = new JFrame();
+        SetIcon(); // funkcja ustawiajaca ikone aplikacji
+         /* Wymiary minimalne okna */
+        int MinWidth = 500;
+        int MinHeight = 500;
+        Dimension MinSize = new Dimension(MinWidth, MinHeight);
+        MainFrame.setResizable(false);
+        MainFrame.setMinimumSize(MinSize);
+        MainFrame.setVisible(true);
+        MainFrame.setTitle(AppName);
+        MainFrame.setLayout(null);
+
+        SetButtons();
+        SetButtonsListeners();
+    }
+    public void SetButtonsListeners()
+    {
+        BtConnectToDB.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                SetButtonClicked(BtConnectToDB);
             }
         });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-
-// call onCancel() when cross is clicked
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-// call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
+    private void SetIcon() {
 
-    private void onOK() {
-// add your code here
-        dispose();
     }
-
-    private void onCancel() {
-// add your code here if necessary
-        dispose();
+    private void SetButtonClicked(JButton button)
+    {
+        ButtonClicked = 1;
     }
+    private void SetBtConnectToDB(int width, int height, String caption) {
+        int WinWidth = MainFrame.getWidth();
+        int WinHeight = MainFrame.getHeight();
 
-    /*public static void main(String[] args) {
-        Gui dialog = new Gui();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
-    }*/
+        BtConnectToDB = new JButton(caption);
+        BtConnectToDB.setLayout(null);
+        MainFrame.add(BtConnectToDB);
+        BtConnectToDB.setSize(265, 25);
+        BtConnectToDB.setLocation(WinWidth/2-width/2, 430);
+        BtConnectToDB.setVisible(true);
+    } // Ustawienia przycisku łączenia się z bazą danych
+    private void SetButtons() {
+        //Przycisk 1, odpowiadajacy za laczenie sie z baza danych
+        SetBtConnectToDB(265,25, "Zaloguj się");
+
+        //-------------------------------
+        /*BtConnect.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                BtConnect.setVisible(false);
+            }
+        });*/
+    }
 }
